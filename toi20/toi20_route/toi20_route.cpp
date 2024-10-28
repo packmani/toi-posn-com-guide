a1f1225ce86550155b966a2713a6458e87146b7a
packmani <40173086+packmani@users.noreply.github.com>
2024-10-28 13:04:09 +0700

    Create toi20_route.cpp

---
+++ toi20/toi20_route/toi20_route.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,num;
+vector<int> weights;
+vector<pair<int,int>> route(int N,vector<int> W) {
+  vector<pair<int,int>> result;
+  int edges = W.size();
+  int nodes = 1;
+  vector<int> curnodes;
+
+  //greedily try to stick as many nodes together until the unused nodes form a tree
+  //edges-result.size() -> edges left, N-nodes -> edges required to form a tree
+  while(edges-result.size()>N-nodes)
+  {
+    curnodes.push_back(nodes);
+    for(int i=1;i<curnodes.size();i++)
+    {
+      result.push_back({i,nodes});
+      if(edges-result.size()==N-nodes) break;
+    }
+    nodes++;
+  }
+
+  //connect to other nodes to form a tree
+  for(int i=nodes;i<N;i++)
+  {
+    result.push_back({i,i+1});
+  }
+  return result;
+}
+
+/* for testing
+int32_t main()
+{
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cin >> n >> m;
+  for(int i=0;i<m;i++)
+  {
+    cin >> num;
+    weights.push_back(num);
+  }
+  auto edges = route(n,weights);
+  for(int i=0;i<edges.size();i++)
+  {
+    cout << edges[i].first << ' ' << edges[i].second << '\n';
+  }
+}
+*/
