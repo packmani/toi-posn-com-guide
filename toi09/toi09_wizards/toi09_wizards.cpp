ee6f0830153466319ebf4ecf52112d029f6a479f
packmani <40173086+packmani@users.noreply.github.com>
2024-10-31 09:56:01 +0700

    Update toi09_wizards.cpp

--- toi09/toi09_wizards/toi09_wizards.cpp
+++ toi09/toi09_wizards/toi09_wizards.cpp
@@ -1,32 +1,35 @@
 //toi9_wizards
-//meet in the middle + find (binary search)
 #include <cstdio>
 #include <algorithm>
-#include <tuple>
 using namespace std;
-int x[4][1500], y[4][1500];
-tuple<int,int,int> v[2250000], tg, *itr;
+int x_a[1500], y_a[1500], x_b[1500], y_b[1500], x_c[1500], y_c[1500];
+pair<int,int> v[2250000], tg, *itr;
 signed main(){
     int xt, yt, n, m = 0; scanf("%d %d %d", &xt, &yt, &n);
-    for(int t = 0; t<4; ++t){
-        for(int i = 0; i<n; ++i) scanf("%d %d", &x[t][i], &y[t][i]);
-    }
-    for(int i = 0; i<n; ++i){
-        for(int j = 0; j<n; ++j){
-            v[m++] = make_tuple(x[0][i] + x[1][j], y[0][i] + y[1][j], i); // a, b
+    for(int i = 0; i<n; ++i) scanf("%d %d", x_a+i, y_a+i);
+    for(int j = 0; j<n; ++j){
+        scanf("%d %d", x_b+j, y_b+j);
+        for(int i = 0; i<n; ++i){
+            v[m++] = make_pair(x_a[i] + x_b[j], y_a[i] + y_b[j]);
         }
-    }
+    } 
     sort(v, v+m); m = unique(v, v+m) - v;
-    for(int i = 0; i<n; ++i){
-        for(int j = 0; j<n; ++j){
-            tg = make_tuple(xt - x[2][i] - x[3][j], yt - y[2][i] - y[3][j], -1); // c, d
-            itr = lower_bound(v, v+m, tg); //search ans
-            if(itr != v+m){
-                int f = get<0>(tg), s = get<1>(tg);
-                int ff = get<0>(*itr), ss = get<1>(*itr), ii = get<2>(*itr);
-                if(f == ff && s == ss){
-                    printf("%d %d\n%d % d\n%d %d\n%d %d", x[0][ii], y[0][ii], f - x[0][ii], s - y[0][ii], x[2][i], y[2][i], x[3][j], y[3][j]);
-                    return 0; //end immediately if answer found
+    for(int k = 0; k<n; ++k) scanf("%d %d", x_c+k, y_c+k);
+    for(int l = 0, x_d, y_d; l<n; ++l){
+        scanf("%d %d", &x_d, &y_d);
+        for(int k = 0; k<n; ++k){
+            tg = make_pair(xt - x_d - x_c[k], yt - y_d - y_c[k]);
+            itr = lower_bound(v, v+m, tg);
+            if(itr != v+m && *itr == tg){
+                int sum_x = tg.first, sum_y = tg.second;
+                for(int i = 0; i<n; ++i){
+                    int rem_x = sum_x - x_a[i], rem_y = sum_y - y_a[i];
+                    for(int j = 0; j<n; ++j){
+                        if(x_b[j] == rem_x && y_b[j] == rem_y){
+                            printf("%d %d\n%d %d\n%d %d\n%d %d", x_a[i], y_a[i], x_b[j], y_b[j], x_c[k], y_c[k], x_d, y_d);
+                            return 0;
+                        }
+                    }
                 }
             }
         }
