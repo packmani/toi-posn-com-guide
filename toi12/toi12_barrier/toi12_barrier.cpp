b116d5f492891af63c6c92b68fb348847d581abf
packmani <40173086+packmani@users.noreply.github.com>
2024-10-31 09:57:47 +0700

    Create toi12_barrier.cpp

---
+++ toi12/toi12_barrier/toi12_barrier.cpp
@@ -0,0 +1,22 @@
+//toi12_barrier
+//sliding window minimum using deque
+#include <cstdio>
+#include <queue>
+using namespace std;
+int qs[6000000];
+signed main(){
+    int n, w, prf_sum = 0, mx = 0, sz = 0; scanf("%d %d", &n, &w);
+    deque<int> dq;
+    for(int i = 0, v; i<n; ++i){
+        scanf("%d", &v); prf_sum += v; qs[i] = prf_sum;
+        if(i < w && (qs[i] > mx || (qs[i] == mx && i+1 < sz))){mx = qs[i]; sz = i+1;}
+        while(!dq.empty() && dq.front() < i-w) dq.pop_front();
+        if(!dq.empty()){
+            int F = dq.front(), tmp = qs[i] - qs[F];
+            if(tmp > mx || (tmp == mx && i-F < sz)){mx = tmp; sz = i-F;}
+        }
+        while(!dq.empty() && qs[dq.back()] >= qs[i]) dq.pop_back();
+        dq.push_back(i);
+    }
+    printf("%d\n%d", mx, sz);
+}
