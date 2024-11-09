e4473a3f1da5a7f335aab551edf17cbef8f3cf0d
packmani <40173086+packmani@users.noreply.github.com>
2024-11-09 22:23:01 +0700

    Update toi18_shopping.cpp

--- toi18/toi18_shopping/toi18_shopping.cpp
+++ toi18/toi18_shopping/toi18_shopping.cpp
@@ -1,31 +1,33 @@
-//toi18_shopping
-#include <cstdio>
-int qs[2][500000];
-int sum(int t, int l, int r){
-    return qs[t][r] - ((l)? qs[t][l-1] : 0);
-}
-signed main(){
-    int n, m; scanf("%d %d", &n, &m);
-    for(int i = 0, x; i<n; ++i){
-        scanf("%d", &x);
-        if(x < 0) qs[1][i] = -x;
-        else qs[0][i] = x;
-        if(i){qs[0][i] += qs[0][i-1]; qs[1][i] += qs[1][i-1];}
-    }
-    while(m--){
-        int x, h; scanf("%d %d", &x, &h);
-        int low = x, high = n-1, j = -1;
-        while(low <= high){
-            int mid = low + ((high-low)>>1), tmp = sum(1, x, mid);
-            if(tmp < h){
-                low = mid+1; j = mid;
-            }else if(tmp > h){
-                high = mid-1;
-            }else if(tmp == h){
-                high = mid-1; j = mid;
-            }
-        }
-        if(j == -1){puts("0"); continue;}
-        printf("%d\n", sum(0, x, j));
-    }
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+int n,q;
+int num;
+int s,money;
+int points[500005];
+vector<int> price;
+int32_t main()
+{
+	cin.tie(0); ios_base::sync_with_stdio(false);
+	cin >> n >> q;
+	price.push_back(0);
+	for(int i=1;i<=n;i++)
+	{
+		cin >> num;
+		int temp=0;
+		if(num<=0) temp = abs(num);
+		else points[i]=num;
+
+		points[i]+=points[i-1];
+		price.push_back(price[i-1]+temp);
+		
+	}
+	price.push_back(LLONG_MAX);
+	points[n+1]=points[n];
+	for(int i=0;i<q;i++)
+	{
+		cin >> s >> money;
+		int idx = lower_bound(price.begin()+s,price.end(),money+price[s])-price.begin();
+		cout << points[idx] - points[s] << '\n';
+	}
 }
